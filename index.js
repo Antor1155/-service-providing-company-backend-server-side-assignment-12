5e1af63c7c3fc48b99919ef4310f1ddb1736c8e4
Md Antor <md.antor1155@gmail.com>
2022-05-28 18:49:05 +0800

    api for adding user to database done

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ObjectId } = require('mongodb');
+require('dotenv').config()
+
+//cross origin resource sharing
+const cors = require('cors');
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+//declearing mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.itp5m.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+// working with api and data collection 
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("CompayBase").collection("products");
+    const userCollection = client.db("CompayBase").collection("users");
+
+    ////api for getting all product
+    app.get('/', async (req, res) => {
+     
+
+      const products = "hello form server"
+
+      res.send({products});
+    });
+    app.get('/product', async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+
+      const products = await cursor.toArray();
+
+      res.send(products);
+    });
+
+    //// api for getting all users
+    app.get('/users', async (req, res) => {
+      const query = {};
+      const cursor = userCollection.find(query);
+
+      const products = await cursor.toArray();
+
+      res.send(products);
+    });
+
+   
+
+
+    ///// api for deleting one item 
+    app.delete("/deleteUser/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // api for adding one item
+    app.post("/addUser", async (req, res) => {
+      const doc = req.body;
+      const result = await userCollection.insertOne(doc);
+      console.log(doc);
+      res.send(result);
+    })
+
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+
+run().catch(console.dir);
+
+
+app.listen(port, () => {
+    console.log("listening to port: ", port);
+})
