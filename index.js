af8a72e46f44a6884c80a25fb3b8670f975b6757
Md Antor <md.antor1155@gmail.com>
2022-05-28 23:10:33 +0800

    added api for posting item

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function run() {
     })
 
      ///// api for posting item
-     app.delete("/deleteUser/:id", async (req, res) => {
+     app.post("/addItem", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await userCollection.deleteOne(query);
